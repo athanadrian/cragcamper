2c24c8744b5021fccd7bb9455a99fc1d90b4823c
athanadrian <athanadrian@gmail.com>
2019-11-13 12:42:56 +0200

    fix

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const colors = require('colors');
-const cookieParser = require('cookie-parser');
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const rateLimit = require('express-rate-limit');
-const hpp = require('hpp');
-const cors = require('cors');
-const fileupload = require('express-fileupload');
-const connectDB = require('./config/db');
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+const fileupload = require("express-fileupload");
+const connectDB = require("./config/db");
 
 // Get env variables
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 //Connect to database
 connectDB();
 
 //Mount middlewares
-const logger = require('./middleware/logger');
-const errorHandler = require('./middleware/error');
+const logger = require("./middleware/logger");
+const errorHandler = require("./middleware/error");
 
 //Import Routes
-const bootcamps = require('./routes/bootcamps');
-const courses = require('./routes/courses');
-const crags = require('./routes/crags');
-const auth = require('./routes/auth');
-const users = require('./routes/users');
-const reviews = require('./routes/reviews');
+const bootcamps = require("./routes/bootcamps");
+const courses = require("./routes/courses");
+const auth = require("./routes/auth");
+const users = require("./routes/users");
+const reviews = require("./routes/reviews");
 
 const app = express();
 
@@ -63,23 +62,22 @@ app.use(cors());
 // Cookie parser
 app.use(cookieParser());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // File uploading
 app.use(fileupload());
 
 // Use routes
-app.use('/api/v1/bootcamps', bootcamps);
-app.use('/api/v1/courses', courses);
-app.use('/api/v1/crags', crags);
-app.use('/api/v1/users', users);
-app.use('/api/v1/reviews', reviews);
-app.use('/api/v1/auth', auth);
+app.use("/api/v1/bootcamps", bootcamps);
+app.use("/api/v1/courses", courses);
+app.use("/api/v1/users", users);
+app.use("/api/v1/reviews", reviews);
+app.use("/api/v1/auth", auth);
 
 // Use middlewares
 app.use(logger);
@@ -94,7 +92,7 @@ const server = app.listen(PORT, () =>
 );
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
+process.on("unhandledRejection", (err, promise) => {
   console.log(`Error ${err.message}`.red);
   // Close Server & exit process
   server.close(() => process.exit(1));
